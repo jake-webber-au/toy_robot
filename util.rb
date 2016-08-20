f69857d849dfa49930a48eead3c70011e103be64
Jake Webber <jake.webber.au@gmail.com>
2016-08-20 21:44:39 +1000

    Symbolising the JSON hash to make it easier when compiling args

--- util.rb
+++ util.rb
@@ -5,7 +5,7 @@ class Utilities
   # pass a path to a JSON command definition.
   def self.load_json_commands(path)
     file = File.read(path)
-    instructions = JSON.parse(file)
+    instructions = JSON.parse(file, symbolize_names: true)
     return instructions
   end
 
