9a269cd5edebba402e73d885af8a5dae703383a3
Jake Webber <jake.webber.au@gmail.com>
2016-08-20 21:43:24 +1000

    Updated sample data and run utility to include option spec

--- run.rb
+++ run.rb
@@ -1,11 +1,13 @@
 require_relative 'util.rb'
-require_relative 'robot.rb'
+require_relative 'Robot.rb'
+require_relative 'CommandRunner.rb'
 
 # load commands via JsonCmdUtil
 instructions = Utilities.load_json_commands(ARGV[0])
 
 # create our robot and issue commands.
-robot = Robot.new
-instructions["commands"].each do |step|
-  robot.issue_command(step)
+robot = Robot.new(Table.new)
+cr = CommandRunner.new(robot)
+instructions[:commands].each do |step|
+  cr.issue_command(step)
 end
